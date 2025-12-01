aed6ccc60fc5642290f0e04f9f0cb0f7b146f3f7
PouyanNorouzi <irfn7pouyan@gmail.com>
2025-11-30 23:23:06 -0800

    changed the dependancy command

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -28,7 +28,7 @@ fi
 
 # Install dependencies if node_modules is not present or package.json changed
 echo "Installing dependencies..."
-bun install --production --force
+bun install --production
 
 # Set proper permissions
 chown -R ec2-user:ec2-user /home/ec2-user/socket-server
